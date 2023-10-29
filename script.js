7661fde6303f7c2a9504c051d4dd7c4b77da282d
ValentinLupu21 <142312818+ValentinLupu21@users.noreply.github.com>
2023-10-29 18:50:33 +0200

    Add files via upload
    buton login adaugat si functional

--- script.js
+++ script.js
@@ -24,3 +24,35 @@ link2.addEventListener('click', () => {
 link3.addEventListener('click', () => {
     scrollToElement('.column');
 });
+
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+
+const scrollY = document.body.style.top;
+
+
+const openModal = function(){
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    document.body.style.overflow = 'hidden';
+    document.body.style.top = `-${window.scrollY}px`;
+}
+
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function(){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+    document.body.style.overflow = '';
+    document.body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);  
+    
+}
+
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
